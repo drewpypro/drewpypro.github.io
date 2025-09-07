4848851a94bc59fbca438033b8a84dcdb6eea9c8
drewpypro <83561525+drewpypro@users.noreply.github.com>
2025-09-06 20:03:08 -0700

    commit before code agent

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/mnt/c/Users/drew/.ideaProjects/drewpypro.github.io/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/root/.yarn/berry/cache/infima-npm-0.2.0-alpha.45-b8be224960-10c0.zip/node_modules/infima/dist/css/default/default.css"),
+  require("/mnt/c/Users/drew/.ideaProjects/drewpypro.github.io/.yarn/__virtual__/@docusaurus-theme-classic-virtual-69bd5307d2/7/root/.yarn/berry/cache/@docusaurus-theme-classic-npm-3.8.1-ebeb2acf49-10c0.zip/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/mnt/c/Users/drew/.ideaProjects/drewpypro.github.io/.yarn/__virtual__/@docusaurus-theme-classic-virtual-69bd5307d2/7/root/.yarn/berry/cache/@docusaurus-theme-classic-npm-3.8.1-ebeb2acf49-10c0.zip/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/mnt/c/Users/drew/.ideaProjects/drewpypro.github.io/src/css/custom.css"),
+];
